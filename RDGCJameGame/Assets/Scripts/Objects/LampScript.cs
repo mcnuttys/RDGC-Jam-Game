f82910e8105ffc6f17ba5571b1080197638dbb6c
Ike McNutt (RIT Student) <irm8232@ad.rit.edu>
2019-11-02 12:08:10 -0400

    Mirror

---
+++ RDGCJameGame/Assets/Scripts/Objects/LampScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LampScript : MonoBehaviour
+{
+
+    private Transform player;
+
+    public float visibilityRange = 25f;
+    public Bounds checkBox;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector3.Distance(transform.position, player.position) < 25f && !Physics.CheckBox(transform.position + checkBox.center, checkBox.size/4))
+        {
+            if (!gameObject.activeSelf)
+                gameObject.SetActive(true);
+            
+        } else
+        {
+            if (gameObject.activeSelf)
+                gameObject.SetActive(false);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireCube(transform.position + checkBox.center, checkBox.size/2);
+    }
+}
