3ab53f554dafe999b3cc6b44f909ecbcbc64eea2
Jacob Kemp-Schneider (RIT Student) <jmk6152@ad.rit.edu>
2019-11-02 10:11:35 -0400

    added key functionality

---
+++ RDGCJameGame/Assets/Scripts/Keys/KeyManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyManager : MonoBehaviour
+{
+    public int keyCount;
+    bool win;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        keyCount = 0;
+        win = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(keyCount>=4)
+        {
+            win = true;
+        }
+    }
+}
---
+++ RDGCJameGame/Assets/Scripts/Keys/KeyPickup.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyPickup : MonoBehaviour
+{
+    public bool pickup;
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pickup = false;
+        player = GameObject.FindGameObjectWithTag("Character");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject == player)
+        {
+            player.GetComponent<KeyManager>().keyCount++;
+            Destroy(gameObject);
+        }
+    }
+}
