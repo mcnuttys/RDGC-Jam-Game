a1ab34e1a5f048613128a7d13c078f86f81fb313
Jacob Kemp-Schneider (RIT Student) <jmk6152@ad.rit.edu>
2019-11-02 17:09:25 -0400

    camerasway

--- RDGCJameGame/Assets/Scripts/Player/CameraSway.cs
+++ RDGCJameGame/Assets/Scripts/Player/CameraSway.cs
@@ -4,19 +4,82 @@ using UnityEngine;
 
 public class CameraSway : MonoBehaviour
 {
-    public float swaySpeed = 3;
-
-    float lastMouse;
+    float timer;
+    float maxTime = 1;
+    bool left;
+    bool up;
+    Vector3 position;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        left = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (left)
+        {
+            if (up)
+            {
+                if (timer > maxTime)
+                {
+                    timer = 0;
+                    left = false;
+                }
+                else
+                {
+                    timer += Time.deltaTime;
+                    position.x += .005f;
+                    gameObject.transform.position = position;
+                }
+            }
+            else
+            {
+                if (timer > maxTime)
+                {
+                    timer = 0;
+                    up = false;
+                }
+                else
+                {
+                    timer += Time.deltaTime;
+                    position.y += .005f;
+                    gameObject.transform.position = position;
+                }
+            }
+        }
+        else
+        {
+            if (up)
+            {
+                if (timer > maxTime)
+                {
+                    timer = 0;
+                    left = true;
+                }
+                else
+                {
+                    timer += Time.deltaTime;
+                    position.x -= .005f;
+                    gameObject.transform.position = position;
+                }
+            }
+            else
+            {
+                if (timer > maxTime)
+                {
+                    timer = 0;
+                    up = true;
+                }
+                else
+                {
+                    timer += Time.deltaTime;
+                    position.y -= .005f;
+                    gameObject.transform.position = position;
+                }
+            }
+        }
     }
 }
