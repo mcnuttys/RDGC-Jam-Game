8e4c08c00469c95d32e537ba000cf0e0978bf816
Ike McNutt (RIT Student) <irm8232@ad.rit.edu>
2019-11-01 20:24:00 -0400

    Player Movement
    Threw together basic player movment, using the Charactor Controller.

---
+++ RDGCJameGame/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private CharacterController cc;
+    private Vector3 direction;
+    private float verticalSpeed;
+
+    public float movementSpeed = 3;
+    public float jumpSpeed = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        direction = transform.rotation * new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        if (direction.magnitude > 1)
+            direction = direction.normalized;
+
+        if(Input.GetButtonDown("Jump") && cc.isGrounded)
+        {
+            verticalSpeed = jumpSpeed;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 move = direction * movementSpeed;
+        move.y = verticalSpeed;
+
+        if (!cc.isGrounded)
+            verticalSpeed += Physics.gravity.y * Time.deltaTime;
+
+        move *= Time.deltaTime;
+        cc.Move(move);
+    }
+}
