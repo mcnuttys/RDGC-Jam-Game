c4e081c18bcf2c84dca2c30fe54dd3695701b393
Jacob Kemp-Schneider (RIT Student) <jmk6152@ad.rit.edu>
2019-11-02 15:28:14 -0400

    fixed footprints again again again again send help

--- RDGCJameGame/Assets/Scripts/Player/Footprints.cs
+++ RDGCJameGame/Assets/Scripts/Player/Footprints.cs
@@ -8,23 +8,30 @@ public class Footprints : MonoBehaviour
     public double timer;
     public double maxTime = 1;
     public GameObject parent;
+    public GameObject walker;
+    public Vector3 previousPosition;
     // Start is called before the first frame update
     void Start()
     {
+        previousPosition = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(timer>maxTime)
+        if (transform.position != previousPosition)
         {
-            timer = 0;
-            Instantiate(footprint, new Vector3(transform.position.x, 1 ,transform.position.z), transform.rotation, parent.transform);
-            //  make walking sound
+            if (timer > maxTime)
+            {
+                timer = 0;
+                Instantiate(footprint, new Vector3(transform.position.x, 1, transform.position.z), transform.rotation, parent.transform);
+                //  make walking sound
+            }
+            else
+            {
+                timer += Time.deltaTime;
+            }
         }
-        else
-        {
-            timer += Time.deltaTime;
-        }
+        previousPosition = transform.position;
     }
 }
