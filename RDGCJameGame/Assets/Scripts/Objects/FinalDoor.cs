7a26c67047325c4d8e77da1a732e9bcc3e0c4881
Jacob Kemp-Schneider (RIT Student) <jmk6152@ad.rit.edu>
2019-11-02 18:13:15 -0400

    added victory door

---
+++ RDGCJameGame/Assets/Scripts/Objects/FinalDoor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FinalDoor : MonoBehaviour
+{
+    public GameObject player;
+    public bool win;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        win = player.GetComponent<KeyManager>().win;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadScene(2); //win screen
+    }
+}
