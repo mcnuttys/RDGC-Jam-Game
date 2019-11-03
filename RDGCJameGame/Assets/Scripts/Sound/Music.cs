e5591fbb15c7eedd52e936e1c30289178ada793e
Jacob Kemp-Schneider (RIT Student) <jmk6152@ad.rit.edu>
2019-11-03 10:00:00 -0500

    implementing player footsteps

---
+++ RDGCJameGame/Assets/Scripts/Sound/Music.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Music : MonoBehaviour
+{
+    GameObject[] musicManagerList;
+    Scene currentScene;
+    bool endPlaying;
+    bool mainPlaying;
+    public AudioSource mainPlayer;
+    public AudioSource endPlayer;
+    public float timer;
+    public float maxTime = 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        endPlaying = false;
+        mainPlaying = false;
+        timer = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void FixedUpdate()
+    {
+        currentScene = SceneManager.GetActiveScene();
+        int currentIndex = currentScene.buildIndex;
+        if ((currentIndex == 0 || currentIndex == 2) && !mainPlaying)
+        {
+            endPlaying = false;
+            if (endPlayer.volume > 0)
+            {
+                endPlayer.volume -= .01f;
+            }
+            else
+            {
+                endPlayer.Stop();
+            }
+            if (timer > maxTime)
+            {
+                timer = 0;
+                mainPlaying = true;
+                mainPlayer.volume = 1;
+                mainPlayer.Play();
+            }
+            else
+            {
+                timer += Time.deltaTime;
+            }
+        }
+        else if (currentIndex == 3 && !endPlaying)
+        {
+            endPlaying = true;
+            endPlayer.volume = 1;
+            endPlayer.Play();
+        }
+        else if (currentIndex == 1)
+        {
+            mainPlaying = false;
+            if (mainPlayer.volume > 0)
+            {
+                mainPlayer.volume -= .01f;
+            }
+            else
+            {
+                mainPlayer.Stop();
+            }
+        }
+    }
+
+    private void Awake()
+    {
+        musicManagerList = GameObject.FindGameObjectsWithTag("music");
+
+        if(musicManagerList.Length>1)
+        {
+            Destroy(this.gameObject);
+        }
+        DontDestroyOnLoad(this.gameObject);
+
+    }
+}
